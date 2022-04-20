6300e7c51a4ed053027bc4330857d06976bb69c7
Busna27 <lucabusnelli2001@gmail.com>
2022-04-20 15:12:44 +0200

    snack 3 - done

--- phpsnack3/index.php
+++ phpsnack3/index.php
@@ -1,5 +1,42 @@
 <?php
-    
+    $posts = [
+        '10/01/2019' => [
+            [
+                'title' => 'Post 1',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 1'
+            ],
+            [
+                'title' => 'Post 2',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 2'
+            ],
+        ],
+        '10/02/2019' => [
+            [
+                'title' => 'Post 3',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 3'
+            ]
+        ],
+        '15/05/2019' => [
+            [
+                'title' => 'Post 4',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 4'
+            ],
+            [
+                'title' => 'Post 5',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 5'
+            ],
+            [
+                'title' => 'Post 6',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 6'
+            ]
+        ],
+    ];
 ?>
 
 
@@ -10,9 +47,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP - Snack 2</title>
+    <title>PHP - Snack 3</title>
 </head>
 <body>
-    
+    <ul>
+        <?php
+            $postsValues = array_values($posts);
+            $postsKeys = array_keys($posts);
+            for ($i = 0 ; $i < count ($postsKeys) ; $i++) { ?>
+                <h3>Post in data: <?php echo $postsKeys[$i] ?></h3>
+                <?php for ($x=0; $x < count($postsValues[$i]); $x++) { ?>
+                    <p>
+                    <?php echo $postsValues[$i][$x]['title']; ?>
+                    </p>
+                    <p>
+                    <?php echo $postsValues[$i][$x]['author']; ?>
+                    </p>
+                    <p>
+                    <?php echo $postsValues[$i][$x]['text']; ?>
+                    </p>
+                <?php } ?>
+            <?php } ?>
+        </ul>
 </body>
 </html>
